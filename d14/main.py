861f4d9b297c3ec4399a299f2004ef6509160f62
ancoulon <ancoulon@student.s19.be>
2021-12-14 12:43:01 +0100

    d14 part 1

---
+++ d14/main.py
@@ -0,0 +1,36 @@
+from os import TMP_MAX
+from typing import Counter
+
+
+def parse_input(path: str = "input.x=txt") -> tuple[str, dict[str, str]]:
+    template: str = str()
+    insertions: dict[str, str] = dict()
+    with open(path, "r") as f:
+        lines = f.read().splitlines()
+        template = lines[0]
+        for line in lines[2:]:
+            k, v = line.split(" -> ")
+            insertions[k] = v
+
+    return template, insertions
+
+
+def apply_step(template: str, insertions: dict[str, str]) -> str:
+    new_template: str = str()
+    for i, c in enumerate(template):
+        new_template += c
+        if i < len(template) - 1:
+            new_template += insertions[c + template[i+1]]
+
+    return new_template
+
+
+
+if __name__ == "__main__":
+    template, insertions = parse_input("input.txt")
+    for i in range(10):
+        template = apply_step(template, insertions)
+        print(i)
+    count = Counter(template)
+    print(count.most_common(1)[0][1] - count.most_common()[-1][1])
+
