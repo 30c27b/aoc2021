12a4a34a3796552c67e4baa6d68bb9264556cac9
ancoulon <ancoulon@student.s19.be>
2021-12-17 17:22:41 +0100

    d15 p

--- d15/main.py
+++ d15/main.py
@@ -1,46 +1,61 @@
 from queue import PriorityQueue
-import os
-import sys
+
 
 def parse_input(path: str = "input.txt") -> list[list[int]]:
-    matrix:  list[list[int]] = list()
+    risk:  list[list[int]] = list()
     with open(path, "r") as f:
         for line in f.read().splitlines():
-            matrix.append([int(c) for c in line])
+            risk.append([int(c) for c in line])
 
-    return matrix
-
-
-def init_queue(matrix: list[list[int]]):
-
-    visited: list[tuple[int, int]] = []
-    risk: list[list[int]] = [[-1 for x in range(len(line))] for line in matrix]
-
-    visit((0, 0), matrix[0][0], risk, visited, matrix)
-    
-    print(risk)
+    return risk
 
 
-def visit(pos: tuple[int, int], current_risk: int, risk: list[list[int]], visited: list[tuple[int, int]], matrix: list[list[int]]):
-    if pos in visited:
+def set_totalrisk(current: int, point: tuple[int, int], closedl: list[list[int]], risk: list[list[int]]) -> bool:
+    x, y = point
+    if x < 0 or x >= len(risk) or y < 0 or y >= len(risk[0]):
         return
-    x, y = pos
-    if x < 0 or x >= len(matrix) or y < 0 or y >= len(matrix[0]):
-        return
-    print(x, y)
-    r = current_risk + matrix[x][y]
-    if risk[x][y] >= 0:
-        risk[x][y] = min(r, risk[x][y])
+    r = current + risk[x][y]
+
+    if closedl[x][y] == -1:
+        closedl[x][y] = r
+    elif r < closedl[x][y]:
+        closedl[x][y] = r
     else:
-        risk[x][y] = r
-    visited.append(pos)
+        return False
+    return True
 
-    visit((x, y-1), r, risk, visited, matrix)  # top
-    visit((x+1, y), r, risk, visited, matrix)  # right
-    visit((x, y+1), r, risk, visited, matrix)  # bot
-    visit((x-1, y), r, risk, visited, matrix)  # left
+
+def astar(risk: list[list[int]]):
+    openl = PriorityQueue()
+    closedl: list[list[int]] = [
+        [-1 for x in range(len(line))] for line in risk]
+
+    openl.put((0, 0), 0)
+    closedl[0][0] = 0
+
+    while not openl.empty():
+
+        point = openl.get()
+        x, y = point
+        if point == (len(risk) - 1, len(risk) - 1):
+            print(closedl[x][y])
+            return
+
+        if set_totalrisk(closedl[x][y], (x, y-1), closedl, risk):
+            openl.put((x, y-1), closedl[x][y-1] +
+                      (len(risk) - x) + (len(risk)-(y-1)))
+        if set_totalrisk(closedl[x][y], (x+1, y), closedl, risk):
+            openl.put((x+1, y), closedl[x+1][y] +
+                      (len(risk) - (x + 1)) + (len(risk)-y))
+        if set_totalrisk(closedl[x][y], (x-1, y), closedl, risk):
+            openl.put((x-1, y), closedl[x-1][y] +
+                      (len(risk) - (x-1)) + (len(risk)-y))
+        if set_totalrisk(closedl[x][y], (x, y+1), closedl, risk):
+            openl.put((x, y+1), closedl[x][y+1] +
+                      (len(risk) - x) + (len(risk)-(y+1)))
 
 
 if __name__ == "__main__":
-    matrix = parse_input("input.txt")
-    init_queue(matrix)
+    risk = parse_input("input.txt")
+    risk[0][0] = 0  # risk of start not
+    astar(risk)
