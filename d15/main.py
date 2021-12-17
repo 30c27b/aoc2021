1b3768a2fea6a48e454b64de5f2d89e4306f220f
ancoulon <ancoulon@student.s19.be>
2021-12-17 17:44:01 +0100

    d15 p2

--- d15/main.py
+++ d15/main.py
@@ -10,6 +10,22 @@ def parse_input(path: str = "input.txt") -> list[list[int]]:
     return risk
 
 
+def part2(risk: list[list[int]]) -> list[list[int]]:
+    new_risk: list[list[int]] = []
+
+    for x in range(len(risk)*5):
+        new_risk.append([0 for y in range(len(risk)*5)])
+
+    for x in range(len(new_risk)):
+        for y in range(len(new_risk)):
+            new_risk[x][y] = risk[x % len(risk)][y % len(
+                risk)] + (x // len(risk)) + (y // len(risk))
+            if new_risk[x][y] > 9:
+                new_risk[x][y] = (new_risk[x][y] % 10) + 1
+
+    return new_risk
+
+
 def set_totalrisk(current: int, point: tuple[int, int], closedl: list[list[int]], risk: list[list[int]]) -> bool:
     x, y = point
     if x < 0 or x >= len(risk) or y < 0 or y >= len(risk[0]):
@@ -57,5 +73,5 @@ def astar(risk: list[list[int]]):
 
 if __name__ == "__main__":
     risk = parse_input("input.txt")
-    risk[0][0] = 0  # risk of start not
+    risk = part2(risk)
     astar(risk)
